91e6732563f3fa3b9ce74695856ddf1843b5a118
Thomas Massie <thomas.massie@outlook.com>
2017-10-11 20:29:56 +0200

    Initialisation...
    pdf files and app.R

---
+++ app.R
@@ -0,0 +1,29 @@
+# Thomas M. Massie, 11.10.2017, Zurich
+rm(list = ls())
+
+
+
+# Load libraries.
+library(shiny)
+library(shinythemes)
+library(pdftools)
+library(wordcloud2)
+library(ggrepel)
+library(tm)
+library(stringr)
+library(lsa)
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)   # devtools::install_github("kassambara/ggpubr")
+
+
+
+
+
+
+
+
+
+
+
+
