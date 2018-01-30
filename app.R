638723aa34f1ffd1c7cfd601fef66b97eddd29b3
Thomas Massie <thomas.massie@outlook.com>
2018-01-30 10:28:15 +0100

    mc

--- app.R
+++ app.R
@@ -21,28 +21,100 @@ library(ggpubr)   # devtools::install_github("kassambara/ggpubr")
 # -----------------------------
 # UI
 
+# ------------------------------------------
+# Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("simplex"),
-                titlePanel("Time-descrete logistic population growth"),
+                titlePanel("Analysis of manifestos in Germany's 2017 election"),
                 sidebarLayout(
                   sidebarPanel(
                     # textInput("txt", "Text input:", "text here"),
-                    sliderInput("p.scale", "Relative size:", 
-                                min = 0, 
-                                max = 1, 
-                                value = 0.25,
-                                step = 0.05),
-                    textInput("text", h3("Text input"), 
-                              value = "Enter text...")
+                    selectInput(inputId = "selected.party", 
+                                label = h3("Party:"), 
+                                choices = c("CDU/CSU", "SPD", "GRUENE", "FDP", "LINKE", "AFD"), 
+                                selected = NULL, multiple = FALSE,
+                                selectize = TRUE, width = NULL, size = NULL),
+                    textInput("txt.1", label = "1st term", value = "Which term...?"),
+                    textInput("txt.1", label = "2nd term", value = "Which term...?"),
+                    textInput("txt.1", label = "3rd term", value = "Which term...?"),
+                    verbatimTextOutput("default")
                   ),
                   mainPanel(
-                    plotOutput(outputId = "figure")
+                    plotOutput(outputId = "figure1"),
+                    plotOutput(outputId = "figure2")
                   )
                 )
 )
 
 
 
+
 server <- function(input, output) {
+  
+  # Arbeitsverzeichnis festlegen.
+  setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Data analysis/Wahlprogramme")
+  
+  # Laden der pdf-Dateie.
+  ifelse(input$selected.party = "CDU/CSU",
+         text_raw <- pdf_text("AFD.pdf"))
+  
+  
+  
+  text_raw <- pdf_text("AFD.pdf")
+  
+  
+  
+  str(text_raw)
+  
+  review_source <- VectorSource(text_raw)
+  
+  # Removeing a couple of super frequent words.
+  remove.words <- c("bundestagswahl",
+                    "wahl",
+                    "wahlprogramm",
+                    "dass")
+  
+  
+  
+  
+  
+  # 1st figure: word cloud.
+  output$figure1 <- renderPlot({
+    wordcloud(text_process$words[1:n.words], text_process$frequency[1:n.words],
+              scale = c(18, 0.5),
+              # colors = c("#666666", "#1172B0")[factor(text_process$isit[1:n.words])],  # Afd
+              colors = c("#666666", "#3E6AA0")[factor(text_process$isit[1:n.words])],  # SPD
+              use.r.layout = FALSE,
+              rot.per = .2,
+              random.order = FALSE,
+              ordered.colors=TRUE,
+              family = "serif", font = 3)
+  })
+  
+  # 2nd figure: bar plot.
+  output$figure2 <- renderPlot({
+    text_process[1:20,] %>%
+      group_by(isit) %>%
+      ggplot() +
+      aes(x = reorder(words, frequency), 
+          y = frequency,
+          fill = isit) +
+      scale_fill_manual(values = c("#666666", "#1172B0")) +
+      geom_col() +
+      theme_classic() +
+      xlab("") +
+      ylab("") +
+      theme(axis.text = element_text(size = 50),
+            axis.line = element_blank(),
+            axis.text.x = element_blank(),
+            axis.ticks.y = element_blank()) +
+      geom_text(aes(label = frequency),
+                size = 14,
+                color = "white",
+                position = position_dodge(width = 0.9),
+                hjust = 1.2) +
+      theme(legend.position="none") +
+      coord_flip()
+  })
 }
 
 
